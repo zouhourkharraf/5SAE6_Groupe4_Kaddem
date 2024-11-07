140cdd4b3b10098ff58d8f8cbdaa1342fea51836
cadevaccon <50071821+cadevaccon@users.noreply.github.com>
2024-11-07 23:06:04 +0100

    fgf

--- src/test/java/KaddemApplicationTests.java
+++
@@ -1,8 +0,0 @@
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import tn.esprit.spring.kaddem.KaddemApplication;
-
- public class KaddemApplicationTests {
-
-
-}
